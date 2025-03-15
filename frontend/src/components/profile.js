23ca29bc11d045820163ba709cbe3e68df50848f
HungPham097 <141068446+HungPhma@users.noreply.github.com>
2025-03-15 01:48:40 -0700

    create a table to show salary, tip, total

--- frontend/src/components/profile.js
+++ frontend/src/components/profile.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, {useState}from "react";
 import "../App.css";
-import profile from "../assests/profile-icon.png";
+import defaultProfile from "../assests/profile-icon.png";
+
 
 const Profile = () => {
+    const [profileImg, setProfileImg] = useState(defaultProfile);
+
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+        if(file){
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setProfileImg(reader.result);
+            };
+            reader.readAsDataURL(file);
+        }
+    };
     return (
         <div className="profile">
             <div className="profile-photo">
-                <img src={profile} alt="User profile" />
+                <img src={profileImg} alt="User profile" />
+                <input 
+                    type="file"
+                    accept="image/*"
+                    id="input-file"
+                    style={{display: 'none'}}
+                    onChange={handleImageChange}
+                />
+                <button className="edit-profile" onClick={() => document.getElementById('input-file').click()}>+</button>
             </div>
             <div className="profile-name">
                 <h3>Hello, Trinh Phan!</h3>
--- frontend/src/components/salaryTable.js
+++ frontend/src/components/salaryTable.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, {useState}from 'react';
 
 const SalaryTable = () => {
+    const [data, setData] = useState([
+        {date: '2023-10-01', salary: 5000, tip: 200, total: 5200},
+        {date: '2023-10-02', salary: 5500, tip: 250, total: 5750},
+        {date: '2023-10-03', salary: 6000, tip: 300, total: 6300},
+        {date: '2023-10-04', salary: 6500, tip: 350, total: 6850},
+        {date: '2023-10-05', salary: 7000, tip: 400, total: 7400},
+    ]);
+
+    
     return (
         <div className='salaryTable'>
-            <h1>Salary Table</h1>
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Salary</th>
+                        <th>Tip</th>
+                        <th>Total</th>
+                        <th>Edit</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {data.map((item, index) => (
+                        <tr key={index}>
+                            <td>{item.date}</td>
+                            <td>{item.salary}</td>
+                            <td>{item.tip}</td>
+                            <td>{item.total}</td>
+                            <td>
+                                {/* <button className="edit-button" onClick={() => handEditSalary(index)}>Edit</button> */}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 };
