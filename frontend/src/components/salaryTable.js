72f57219e3a1a7957c2f46030a420f73d5860012
HungPham097 <141068446+HungPhma@users.noreply.github.com>
2025-03-26 18:45:44 -0700

    add table to github

--- frontend/src/components/salaryTable.js
+++ frontend/src/components/salaryTable.js
@@ -1,15 +1,100 @@
-import React, {useState}from 'react';
+import React, { useState, useEffect }from 'react';
+import Swal from 'sweetalert2';
 
 const SalaryTable = () => {
-    const [data, setData] = useState([
-        {date: '2023-10-01', salary: 5000, tip: 200, total: 5200},
-        {date: '2023-10-02', salary: 5500, tip: 250, total: 5750},
-        {date: '2023-10-03', salary: 6000, tip: 300, total: 6300},
-        {date: '2023-10-04', salary: 6500, tip: 350, total: 6850},
-        {date: '2023-10-05', salary: 7000, tip: 400, total: 7400},
-    ]);
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        const fetchDate =  async () => {
+            try{
+                const response = await fetch('http://localhost:8000/api/salary');
+                const data = await response.json();
+                console.log('Fetched data: ', data);
+                setData(data);
+            }
+            catch(err){
+                console.log('Error fetching data: ', err);
+            }
+        };
 
-    
+        fetchDate();
+    }, []);
+
+    const handEditSalary = async (index) => {
+        const currentIndex = data[index];
+
+        const { value: formValues } = await Swal.fire({
+            title: 'Edit Salary and Tip',
+            html:
+                `<label>Salary:</label>
+                <input type='number' id='salary' value='${currentIndex.salary}' />
+                <label>Tip:</label>
+                <input type='number' id='tip' value='${currentIndex.tip}' />
+                <label>Date:</label>
+                <input type='date' id='date' value='${currentIndex.date}' />`,
+            showCancelButton: true,
+            cancelButtonText: 'Cancel',
+            confirmButtonText: 'Save',
+            preConfirm: () => {
+                const newsalary = parseFloat(document.getElementById('salary').value);
+                const newtip = parseFloat(document.getElementById('tip').value);
+                const newdate = document.getElementById('date').value;
+
+                if(isNaN(newsalary) || isNaN(newtip)){
+                    Swal.showValidationMessage('Please enter a valid number');
+                    return false;
+                }
+                return {newsalary, newtip, newdate};
+            }
+        });
+
+        if(formValues){
+            const {newsalary, newtip, newdate} = formValues;  
+            const id = currentIndex._id;
+            console.log('currently ID:', id);
+            try {
+                const response = await fetch(`http://localhost:8000/api/salary/${id}`, {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        date: newdate,
+                        salary: newsalary,
+                        tip: newtip
+                    })
+                });
+
+                console.log('Response status:', response.status);
+                console.log('Response body:', await response.json());
+
+                if(response.ok){
+                    const updatedSalary = await response.json();
+                    const newData = [...data];
+                    newData[index] = updatedSalary;
+                    newData.sort((a, b) => new Date(a.date) - new Date(b.date));
+
+                    setData(newData);
+
+                    Swal.fire(
+                        'Updated!',
+                        'The salary has been updated.',
+                        'success'
+                    );
+                }
+                else{
+                    Swal.fire(
+                        'Error!',
+                        'Failed to update the database.',
+                        'error'
+                    );
+                }
+            }
+            catch (error) {
+                console.error('Error updating:', error);
+                Swal.fire('Error', 'Failed to connect to server.', 'error');
+            }
+        }
+    };
     return (
         <div className='salaryTable'>
             <table className='table'>
@@ -25,13 +110,13 @@ const SalaryTable = () => {
 
                 <tbody>
                     {data.map((item, index) => (
-                        <tr key={index}>
+                        <tr key={item._id}>
                             <td>{item.date}</td>
                             <td>{item.salary}</td>
                             <td>{item.tip}</td>
                             <td>{item.total}</td>
                             <td>
-                                {/* <button className="edit-button" onClick={() => handEditSalary(index)}>Edit</button> */}
+                                <button className="edit-button" onClick={() => handEditSalary(index)}>Edit</button>
                             </td>
                         </tr>
                     ))}
